6234a4f9d85f4ecb6ea3ff6c9eba87fc22278a25
balisong418 <dekameron_13@mail.ru>
2016-05-23 21:21:11 +0300

    Create task7.py

---
+++ task7.py
@@ -0,0 +1,9 @@
+def task7():
+    result = float(input())/float(input())
+    if (result == result//1):
+        return "true "+str(result)
+    else:
+        return "false "+str(result)
+
+if __name__ == "__main__":
+    print task7()
