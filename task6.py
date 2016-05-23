e92da997f9b51891b4e9ef47392e0d787508f990
balisong418 <dekameron_13@mail.ru>
2016-05-23 21:29:31 +0300

    Update task6.py

--- task6.py
+++ task6.py
@@ -1,9 +1,6 @@
 def task6():
-    number1 = input()
-    number2 = input()
-    if (11<=number1+number2<=19):
-        return number1+number2
-
-
+    summ = input()+input()
+    if (11<=summ<=19):
+        return summ
 if __name__ == "__main__":
     print task6()
